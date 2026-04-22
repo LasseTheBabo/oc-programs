0174c88b6dfe70f92845334636d035d8b65cab10
LasseTheBabo <lasse-schaefers.mail@proton.me>
2026-04-22 13:31:59 +0200

    idk

--- MineOS/PAC.app/components.lua
+++ MineOS/PAC.app/components.lua
@@ -6,7 +6,7 @@ local function init()
   local addresses = component.list("ntm_pa_dipole")
   
   for address in addresses do
-    dipole = component.proxy(address)
+    local dipole = component.proxy(address)
     for i=1,5 do
       if dipole.getThreshold() == i then
         dipoles[i] = dipole.address

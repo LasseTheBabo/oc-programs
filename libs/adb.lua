df69e831a3daae53b2fd895f2d07ff40d58fee42
LasseTheBabo <lasse-schaefers.mail@proton.me>
2026-05-21 16:29:18 +0200

    not android debug bridge

--- libs/adb.lua
+++ libs/adb.lua
@@ -1,10 +1,20 @@
 local component = require("component")
+local event = require("event")
+local os = require("os")
+
+-- Address DataBase
+-- not Android Debug Bridge
 
 local adb = {}
 
-function adb.set(filter)
-    local filteredList = component.list(filter)
-    
+function adb.waitForNewAddress(filter)
+    while true do
+        local _, address, type = event.pull("component_added")
+
+        if not filter or type == filter then
+            return address
+        end
+    end
 end
 
 return adb

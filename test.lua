da5f343c0f907a731868331d8cf93ecf6e2b36f1
LasseTheBabo <lasse-schaefers.mail@proton.me>
2026-05-27 07:03:56 +0200

    api or something

--- test.lua
+++ test.lua
@@ -1,2 +1,19 @@
-local adb = require("adb")
+local internet = require("internet")
+local json = require("json")
 
+local function getServerStatus(server)
+    local handle = internet.request("https://api.mcstatus.io/v2/status/java/" .. server)
+
+    local result = ""
+    for chunk in handle do
+        result = result .. chunk
+    end
+
+    local data = json.decode(result)
+    return data
+end
+
+local players = getServerStatus("penis.nvrsrv.com:50000").players.list
+for _, player in ipairs(players) do
+    print(player)
+end
